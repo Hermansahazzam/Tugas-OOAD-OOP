57badaeb8ae456cb0d4b0d61a6c73557bef7cfb6
Hermansahazzam <32549306+Hermansahazzam@users.noreply.github.com>
2017-10-18 19:07:26 +0700

    Add files via upload

---
+++ Masaknasi.java
@@ -0,0 +1,23 @@
+public class Masaknasi {
+
+     
+    
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Bahan");
+        System.out.println("beras");
+        System.out.println("Air");
+        System.out.println("Rempah");
+        System.out.println("");
+        System.out.println("Cara Memasak");
+        System.out.println("1. Bersihkan beras menggunakan air hingga kotoran yang terlarut bisa hilang");
+        System.out.println("2. Campurakan beras dengan air bersih dengan perbandingan air dan beras 3 :2");
+        System.out.println("3. Masukan campuran beras dan air tadi kedalam RiceCooker");
+        System.out.println("4. Masukan rempah sebagai peningkat rasa");
+        System.out.println("5. colokan kabe; ke sumber listrik");
+        System.out.println("6. posisikan saklar RiceCooker ke posisi Cook");
+        System.out.println("7. Tunggu hingga proses menanak nasi selesai");
+
+    }
+
+}
